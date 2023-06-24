02372f00770c9f04ffb6f47cfb7c586c35d49466
yadavamit2611 <thisisamit.kumar21@gmail.com>
2023-06-24 23:04:14 +0200

    react package changes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1",
 { 
     id:"title",
     className:"myclass",
-},"Heading 1");
+    key:"heading1",
+},"Heading 1 for parcel");
 
 const heading2 = React.createElement("h2",
 { 
     id:"title1",
     className:"myclass",
+    key:"heading2",
 },"Heading 2");
 
 const container = React.createElement(
@@ -22,3 +27,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //async defer
 root.render(container);
+
